5b4346073792ff4243499cf6c164a986b0197152
alfredo <alfredobeckerpt@gmail.com>
2025-06-11 00:14:38 -0400

    vercel deploy conifg.8

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,15 @@
 import express from "express";
-import dotenv from "dotenv";
-import router from "./src/router";
+import router from "../src/router";
 import { VercelRequest, VercelResponse } from '@vercel/node';
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(router);
 
-// 404 handler
 app.use((req, res) => {
   res.status(404).json({ error: "Route not found" });
 });
 
-// Export as Vercel handler
 export default (req: VercelRequest, res: VercelResponse) => {
   app(req, res);
 };
